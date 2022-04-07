717a67b562744cde61fb12c8d7734c9fb11c123e
Patrick McRea <90723319+PatMcRea@users.noreply.github.com>
2022-04-07 17:06:06 -0400

    add pause menu layout + fix leaderboard layout

--- app/src/main/java/uqac/dim/gamersguess/QuizActivity.java
+++ app/src/main/java/uqac/dim/gamersguess/QuizActivity.java
@@ -1,6 +1,10 @@
 package uqac.dim.gamersguess;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -63,9 +67,4 @@ public class QuizActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        bd.destroyInstance();
-        super.onDestroy();
-    }
 }
