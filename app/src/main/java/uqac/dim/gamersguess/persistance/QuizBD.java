13d40d6d8d5f9db32b220d841fa8c9d94f6910f8
Ann-Laurie Lapointe <lapointeannlaurie@gmail.com>
2022-04-17 01:46:40 -0400

    Highscore system and adding them to database + good answer is now shown when bad one is selected

--- app/src/main/java/uqac/dim/gamersguess/persistance/QuizBD.java
+++ app/src/main/java/uqac/dim/gamersguess/persistance/QuizBD.java
@@ -6,21 +6,38 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.io.File;
+
 @Database(entities = {Question.class, Reponse.class, Score.class}, version = 1)
 public abstract class QuizBD extends RoomDatabase {
     private static QuizBD INSTANCE;
     public abstract QuizDao quizDao();
 
     public static QuizBD getDatabase(Context context) {
-        if (INSTANCE == null) {
-            INSTANCE =
-            Room.databaseBuilder(context, QuizBD.class, "quiz.db")
-                    .createFromAsset("database/quiz.db")
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build();
+        if (!doesDatabaseExist(context)) {
+            if (INSTANCE == null) {
+                INSTANCE =
+                        Room.databaseBuilder(context, QuizBD.class, "quiz.db")
+                                .createFromAsset("database/quiz.db")
+                                .allowMainThreadQueries()
+                                .fallbackToDestructiveMigration()
+                                .build();
+            }
+        } else {
+            if (INSTANCE == null) {
+                INSTANCE =
+                        Room.databaseBuilder(context, QuizBD.class, "quiz.db")
+                                .allowMainThreadQueries()
+                                .fallbackToDestructiveMigration()
+                                .build();
+            }
         }
         return INSTANCE;
+    }
+
+    private static boolean doesDatabaseExist(Context context) {
+        File dbFile = context.getDatabasePath("quiz.db");
+        return dbFile.exists();
     }
 
     public static void destroyInstance() { INSTANCE = null; }
